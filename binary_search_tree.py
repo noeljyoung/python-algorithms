84af7e6b2146c62d9cedceedee0ee7c4d79950e2
= <noel.young@actindo.com>
2025-12-17 08:06:37 +0100

    BST updated

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -4,7 +4,7 @@ class Tree:
             self.value = value
             self.left = None
             self.right = None
-    
+
     def __init__(self):
         self.root = None
 
@@ -21,14 +21,108 @@ class Tree:
             if new_node.value < current.value: # go left
                 if not current.left:
                     current.left = new_node
+                    return
                 else:
                     current = current.left
             elif new_node.value > current.value: # go right
                 if not current.right:
                     current.right = new_node
+                    return
                 else:
                     current = current.right
+            elif new_node.value == current.value:
+                return;
+
+    def find(self, value):
+        if not self.root:
+            return None
+
+        current = self.root
+
+        while current:
+            leaf = current
+            if value == current.value: # value is found
+                break
+            elif value < current.value: # go left
+                current = current.left
+            elif value > current.value: # go right
+                current = current.right
+
+        return leaf
+
+    def max(self):
+        if not self.root:
+            return None
+
+        current = self.root
+        max = current
+
+        while current:
+            if current.right is not None:
+                max = current.right
+
+            current = current.right
+
+        return max.value
+
+    def min(self):
+        if not self.root:
+            return None
+
+        current = self.root
+        min = current
+
+        while current:
+            if current.left is not None:
+                min = current.left
+            current = current.left
+
+        return min.value
+
+    def predecessor(self, value):
+        if self.root is None:
+            return None
+
+        current = self.root
+
+
+    def __str__(self):
+        if not self.root:
+            return "Empty tree"
+
+        lines = []
+        level = [self.root]
+
+        while level:
+            current_line = []
+            next_level = []
+
+            for node in level:
+                if node:
+                    next_level.extend([node.left, node.right])
+                else:
+                    current_line.append("·")
+                    next_level.extend([None, None])
+
+            if any(next_level):
+                lines.append(" ".join(current_line))
+                level = next_level
+            else:
+                lines.append(" ".join(current_line))
+                break
+
+        return "\n".join(lines)
 
 tree = Tree()
 tree.insert(10)
-print(tree)
+tree.insert(19)
+tree.insert(11)
+tree.insert(9)
+tree.insert(1)
+tree.insert(10)
+# print(tree)
+print(tree.min())
+print(tree.max())
+
+# look = tree.find(10)
+# print(look.value)
